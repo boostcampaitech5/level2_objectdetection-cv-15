d4f00a49eb5d4859359caa7f03669d5c4b81f6a7
JonsSeo Park <justin93820@gmail.com>
2023-05-11 13:55:15 +0000

    Fix : ensemble, EDA code
    - ensemble 코드에서 .csv 파일만 가져오도록 변경
    - EDA 마지막 그래프에서 구간을 30이 아닌 32로 변경
    - 변경된 이유는 원래 0~32, 32~96으로 구간이 32단위로 잘려있기 때문

--- codebook/ensemble_WBF.py
+++ codebook/ensemble_WBF.py
@@ -1,18 +1,23 @@
 from ensemble_boxes import nms, soft_nms, weighted_boxes_fusion
 import pandas as pd
 import os
+from tqdm import tqdm
 
+# codebook 내부 ensemble 폴더에 들어있는 csv파일들을 모두 가져옴
 file_list = os.listdir("./ensemble")
 csv_list = []
 for file_name in file_list :
-    csv_list.append(pd.read_csv(file_name, keep_default_na=False))
-csv_sum = pd.read_csv(file_list[0], keep_default_na=False)
+    if file_name.endswith('.csv'):
+        csv_list.append(pd.read_csv(os.path.join("./ensemble", file_name), keep_default_na=False))
+
+# sum할 pd를 그냥 첫번째 파일에서 형식을 가져온 후 그곳에 덮어씀
+csv_sum = pd.read_csv(os.path.join("./ensemble", file_list[0]), keep_default_na=False)
 
 # WBF hyperparameter 
 iou_thr = 0.5
 skip_box_thr = 0.0001
 
-for img_num in range(csv_sum.shape[0]) :
+for img_num in tqdm(range(csv_sum.shape[0])) :
     boxes = []
     scores = []
     labels = []
@@ -48,4 +53,4 @@ for img_num in range(csv_sum.shape[0]) :
         prediction_string += str(result_box[i][0]) + " " + str(result_box[i][1]) + " " + str(result_box[i][2]) + " " + str(result_box[i][3]) + " "
 
     csv_sum.loc[img_num]["PredictionString"] = prediction_string
-csv_sum.to_csv("./detectron2/output/ensemble_WBF.csv", index=None)
+csv_sum.to_csv("/ensemble_WBF_withRetina.csv", index=None)
